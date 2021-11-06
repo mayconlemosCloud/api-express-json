06674caedea37f16f6d2976174b4d88d53976404
mayconlemosCloud <mayconlemos.riosaude@gmail.com>
2021-11-06 11:25:44 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 
 const express = require('express');
 
+
+
 const routes = require("./routes/routes.js");
 var bodyParser = require('body-parser')
 const PORT = 3009
 const app = express();
+
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,15 +8,15 @@ routes.get("/listar", atendimentosController.listar);
 routes.get("/listarDiariamente", atendimentosController.listarDiariamente);
 routes.get("/listarSemanal", atendimentosController.listarSemanal);
 
-routes.post("/salvar", atendimentosController.salvar);
-routes.post("/salvarDiario", atendimentosController.salvarDiario);
-routes.post("/salvarSemanal", atendimentosController.salvarSemanal);
+routes.post("/salvar", atendimentosController.salvar); //passar pelo boby igual o formato de listar 
+routes.post("/salvarDiario", atendimentosController.salvarDiario); //passar pelo boby igual o formato de listarDiariamente 
+routes.post("/salvarSemanal", atendimentosController.salvarSemanal); //passar pelo boby igual o formato de listarSemanal 
 
 
 
-routes.delete("/deletarDay/:day", atendimentosController.deletarDay);
-routes.delete("/DeletarDiariamente/:day", atendimentosController.DeletarDiariamente);
-routes.delete("/deletarSemanalmente/:day", atendimentosController.deletarSemanalmente);
+routes.delete("/deletarDay/:day", atendimentosController.deletarDay); //passar como paramentro o Day 
+routes.delete("/DeletarDiariamente/:day", atendimentosController.DeletarDiariamente); //passar como paramentro o Day 
+routes.delete("/deletarSemanalmente/:day", atendimentosController.deletarSemanalmente); //passar como paramentro o Day 
 
 
 
